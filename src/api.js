de207ea6765819bb8e37871db13d2eeadf773d80
Mat <mathewdenison@gmail.com>
2025-04-11 13:03:15 -0400

    updates

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://dashboard-service.default.svc.cluster.local:8000/api/user",
-    withCredentials: true, // Include session cookies for authentication
+    baseURL: process.env.REACT_APP_PUBLIC_BASE_URL,
+    withCredentials: true, // Include cookies if necessary
 });
 
 export default API;
